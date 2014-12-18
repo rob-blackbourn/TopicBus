7600088947df6eb82f0859ad8b9d0fb0a9504259
Rob Blackbourn <rob.blackbourn@googlemail.com>
2014-12-18 12:07:51 +0000

    Added a JSON encoder.

---
+++ JetBlack.TopicBus/IO/JsonEncoder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace JetBlack.TopicBus.IO
+{
+    public class JsonEncoder : IByteEncoder
+    {
+        public object Decode(byte[] bytes)
+        {
+            var s = Encoding.UTF8.GetString(bytes);
+            var obj = JsonConvert.DeserializeObject<Dictionary<string,object>>(s);
+            return obj;
+        }
+
+        public byte[] Encode(object obj)
+        {
+            var s = JsonConvert.SerializeObject(obj);
+            var bytes = Encoding.UTF8.GetBytes(s);
+            return bytes;
+        }
+    }
+}
+
